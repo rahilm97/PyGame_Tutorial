eaa4edbfcd04c1b56be6e97fafffe2b2c3752ee4
Rahil Mehta <rahilm97@hotmail.com>
2022-06-30 15:10:23 -0700

    Pauses movement on press of p

--- shaking_tiger.py
+++ shaking_tiger.py
@@ -20,16 +20,24 @@ screen = pygame.display.set_mode(size)
 ball = pygame.image.load("tiger.png")
 ballrect = ball.get_rect()
 
-#Set preferred volume
+# Set preferred volume
 mixer.music.set_volume(0.2)
 
-#Play the music
+# Play the music
 mixer.music.play()
+status = 1 # 1 = active, -1 = paused
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_p: 
+                status = -status
+    
+    if status == -1: 
+        mixer.pause()
+    if status == 1:
+        mixer.unpause()
+        ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
