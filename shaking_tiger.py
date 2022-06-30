4572f61add733d6ab3691e3e1d333b4eb0f2148d
Rahil Mehta <rahilm97@hotmail.com>
2022-06-30 14:17:54 -0700

    Initial commit

---
+++ shaking_tiger.py
@@ -0,0 +1,40 @@
+'''
+Name: Rahil Mehta
+This code is based on the following tutorial, with a different image and added sound:
+https://www.pygame.org/docs/tut/PygameIntro.html
+'''
+import sys
+import pygame
+from pygame import mixer
+
+pygame.init()
+mixer.init()
+mixer.music.load('tigers-roaring.mp3')
+
+size = width, height = 580, 680
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("tiger.png")
+ballrect = ball.get_rect()
+
+#Set preferred volume
+mixer.music.set_volume(0.2)
+
+#Play the music
+mixer.music.play()
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
